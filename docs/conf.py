d1f231471c8e6681dd558c70d5b1af4c2a7570a3
Nikki Prudencio <nikki.prudencio@liferay.com>
2020-06-29 23:12:46 -0700

    LRQA-59275 Setup for Tabs

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -38,7 +39,8 @@ release = u''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark'
+extensions = ['recommonmark',
+    'sphinx_tabs.tabs'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
